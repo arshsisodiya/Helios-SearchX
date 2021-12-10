6833a07058d56b64b5317ee498b2b8419207434b
arshsisodiya <arshsisodiya@gmail.com>
2021-12-10 16:15:01 +0530

    added github action
    now you can deploy bot using repo secrates and github actions on heroku

--- bot/__init__.py
+++ bot/__init__.py
@@ -16,6 +16,16 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
                     level=logging.INFO)
 
+CONFIG_FILE_URL = os.environ.get('CONFIG_FILE_URL', None)
+if CONFIG_FILE_URL is not None:
+    res = requests.get(CONFIG_FILE_URL)
+    if res.status_code == 200:
+        with open('config.env', 'wb+') as f:
+            f.write(res.content)
+            f.close()
+    else:
+        logging.error(f"Failed to download config.env {res.status_code}")
+
 load_dotenv('config.env')
 
 def getConfig(name: str):
